eb44b26cf13dc3eb5310d73e5fc37546a435af42
Nikita Nafranets <eddimensi@gmail.com>
2020-10-27 00:13:22 +0300

    улучшил работу скрипта

--- index.ts
+++ index.ts
@@ -4,11 +4,51 @@ type Project = 'assr' | 'akit'
 type Type = 'feature' | 'bugfix'
 type Args = [Project?, Type?, string?]
 
-const [project, type, number] = Deno.args as Args
+let [project, type, number] = Deno.args as Args
 
-if (!project || !type || !number) {
-  console.log('Ты забыл указать project: %s, type: %s, number: %s', project, type, number)
+const projectPaths = {
+  akit: '../akit/',
+  assr: '../assr/'
+}
+
+if (!project) {
+  console.error('Ты не указал project')
   Deno.exit(1)
+}
+
+if (![type, number].every(Boolean)) {
+  const git = Deno.run({
+    cmd: ['git', 'branch', '--show-current'],
+    cwd: projectPaths[project],
+    stdout: 'piped',
+    stderr: 'piped'
+  })
+  
+  const { code } = await git.status()
+  
+  if (code === 0) {
+    const rawOutput = await git.output()
+    const output = new TextDecoder().decode(rawOutput)
+    const [firstPart, secondPart] = output.trim().split('/')
+    if (['feature', 'bugfix'].includes(firstPart)) {
+      type = firstPart as Type
+    } else {
+      console.log('Не могу распарсить или значения не подходящие', firstPart)
+      Deno.exit(1)
+    }
+
+    if (secondPart.includes('ALFABANKRU-')) {
+      number = secondPart.replace('ALFABANKRU-', '')
+    } else {
+      console.log('Не нашел шаблона', secondPart)
+      Deno.exit(1)
+    }
+  } else {
+    const rawError = await git.stderrOutput();
+    const errorString = new TextDecoder().decode(rawError);
+    console.error(errorString);
+    Deno.exit(1)
+  }
 }
 
 if (project === 'assr') {
